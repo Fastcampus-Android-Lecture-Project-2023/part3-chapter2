53350eb75b7799b5862a13c6df2a24a0d0726803
hyochan <overcome26@gmail.com>
2022-12-26 15:48:45 +0900

    보안키패드 - Shuffle키패드 만들기 2 - Click Listener 연결하기

--- app/src/main/java/fastcampus/part3/chapter2/widget/ShuffleNumberKeyboard.kt
+++ app/src/main/java/fastcampus/part3/chapter2/widget/ShuffleNumberKeyboard.kt
@@ -3,6 +3,7 @@ package fastcampus.part3.chapter2.widget
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.GridLayout
 import android.widget.TextView
 import androidx.core.view.children
@@ -11,20 +12,30 @@ import kotlin.random.Random
 
 class ShuffleNumberKeyboard @JvmOverloads
 constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
-    GridLayout(context, attrs, defStyleAttr) {
+    GridLayout(context, attrs, defStyleAttr) , View.OnClickListener{
 
     private var _binding: WidgetShuffleNumberKeyboardBinding? = null
     private val binding get() = _binding!!
 
+    private var keyPadListener: KeyPadListener? = null
+
     init {
         _binding =
             WidgetShuffleNumberKeyboardBinding.inflate(LayoutInflater.from(context), this, true)
+        binding.view = this
+        binding.clickListener = this
         shuffle()
     }
 
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
         _binding = null
+    }
+
+    override fun onClick(v: View) {
+        if (v is TextView && v.tag != null) {
+            keyPadListener?.onClickNum(v.text.toString())
+        }
     }
 
     fun shuffle() {
@@ -40,5 +51,23 @@ constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
                 keyNumberArr.removeAt(randIndex)
             }
         }
+    }
+
+    fun setKeyPadListener(keyPadListener: KeyPadListener) {
+        this.keyPadListener = keyPadListener
+    }
+
+    fun onClickDelete() {
+        keyPadListener?.onClickDelete()
+    }
+
+    fun onClickDone() {
+        keyPadListener?.onClickDone()
+    }
+
+    interface KeyPadListener {
+        fun onClickNum(num: String)
+        fun onClickDelete()
+        fun onClickDone()
     }
 }
