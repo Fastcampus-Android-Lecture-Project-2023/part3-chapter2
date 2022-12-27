0c2ab583c1b667d8b70d1abc783b4bdc6d8eb83f
hyochan <overcome26@gmail.com>
2022-12-27 23:51:36 +0900

    휴대폰 본인인증 UI 구성 2 - Code 연결하기

--- app/src/main/java/fastcampus/part3/chapter2/IdentityInputActivity.kt
+++ app/src/main/java/fastcampus/part3/chapter2/IdentityInputActivity.kt
@@ -1,7 +1,13 @@
 package fastcampus.part3.chapter2
 
+import android.content.Context
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.core.widget.doAfterTextChanged
 import fastcampus.part3.chapter2.databinding.ActivityIdentityInputBinding
 
 class IdentityInputActivity : AppCompatActivity() {
@@ -13,5 +19,81 @@ class IdentityInputActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityIdentityInputBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initView()
+        binding.nameEdit.showKeyboardDelay()
+    }
+
+    private fun initView() {
+        with(binding) {
+            nameEdit.setOnEditorActionListener(EditorInfo.IME_ACTION_NEXT) {
+                birthdayLayout.isVisible = true
+                birthdayEdit.showKeyboard()
+            }
+
+            birthdayEdit.doAfterTextChanged {
+                if (birthdayEdit.length() > 7) {
+                    genderLayout.isVisible = true
+                    birthdayEdit.closeKeyboard()
+                }
+            }
+
+            telecomChipGroup.setOnCheckedStateChangeListener { group, checkedIds ->
+                if (!phoneLayout.isVisible) {
+                    phoneLayout.isVisible = true
+                    phoneEdit.showKeyboard()
+                }
+            }
+
+            genderChipGroup.setOnCheckedStateChangeListener { group, checkedIds ->
+                if (!telecomLayout.isVisible) {
+                    telecomLayout.isVisible = true
+                }
+            }
+
+            phoneEdit.doAfterTextChanged {
+                if (phoneEdit.length() > 10) {
+                    confirmButton.isVisible = true
+                    phoneEdit.closeKeyboard()
+                }
+            }
+
+            phoneEdit.setOnEditorActionListener(EditorInfo.IME_ACTION_DONE) {
+                if (phoneEdit.length() > 9) {
+                    confirmButton.isVisible = true
+                    phoneEdit.closeKeyboard()
+                }
+            }
+        }
+    }
+
+    private fun EditText.setOnEditorActionListener(action: Int, invoke: () -> Unit) {
+        setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == action) {
+                invoke()
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    private fun EditText.showKeyboard() {
+        this.requestFocus()
+        val inputMethodManager =
+            this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+    }
+
+    private fun EditText.showKeyboardDelay() {
+        postDelayed({
+            showKeyboard()
+        }, 200)
+    }
+
+   private fun EditText.closeKeyboard() {
+        this.clearFocus()
+        val inputMethodManager =
+            this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(this.windowToken, 0)
     }
 }
