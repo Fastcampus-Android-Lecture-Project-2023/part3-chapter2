9d7048295b921de3564b81194ecf65442808409f
hyochan <overcome26@gmail.com>
2022-12-30 18:36:05 +0900

    휴대폰 본인인증 데이터 무결성 체크 - 휴대폰 본인인증 데이터 무결성 체크

--- app/src/main/java/fastcampus/part3/chapter2/IdentityInputActivity.kt
+++ app/src/main/java/fastcampus/part3/chapter2/IdentityInputActivity.kt
@@ -26,15 +26,47 @@ class IdentityInputActivity : AppCompatActivity() {
     private fun initView() {
         with(binding) {
             nameEdit.setOnEditorActionListener(EditorInfo.IME_ACTION_NEXT) {
-                birthdayLayout.isVisible = true
-                birthdayEdit.showKeyboard()
+                if (validName()) {
+                    nameLayout.error = null
+                    if (phoneLayout.isVisible) {
+                        confirmButton.isVisible = true
+                    } else {
+                        birthdayLayout.isVisible = true
+                        birthdayEdit.showKeyboard()
+                    }
+                } else {
+                    confirmButton.isVisible = false
+                    nameLayout.error = "1자이상의 한글을 입력해주세요"
+                }
             }
 
             birthdayEdit.doAfterTextChanged {
                 if (birthdayEdit.length() > 7) {
-                    genderLayout.isVisible = true
-                    birthdayEdit.closeKeyboard()
+                    if (validBirthDay()) {
+                        birthdayLayout.error = null
+                        if (phoneLayout.isVisible) {
+                            confirmButton.isVisible = true
+                        } else {
+                            genderLayout.isVisible = true
+                            birthdayEdit.closeKeyboard()
+                        }
+                    } else {
+                        confirmButton.isVisible = false
+                        birthdayLayout.error = "생년월일 형식이 다릅니다."
+                    }
                 }
+            }
+
+            birthdayEdit.setOnEditorActionListener(EditorInfo.IME_ACTION_DONE) {
+                val isValid = validBirthDay() && birthdayEdit.length() > 7
+                if (isValid) {
+                    confirmButton.isVisible = phoneLayout.isVisible
+                    birthdayLayout.error = null
+                } else {
+                    birthdayLayout.error = "생년월일 형식이 다릅니다."
+                }
+
+                birthdayEdit.closeKeyboard()
             }
 
             telecomChipGroup.setOnCheckedStateChangeListener { group, checkedIds ->
@@ -52,18 +84,42 @@ class IdentityInputActivity : AppCompatActivity() {
 
             phoneEdit.doAfterTextChanged {
                 if (phoneEdit.length() > 10) {
-                    confirmButton.isVisible = true
-                    phoneEdit.closeKeyboard()
+                    if (validPhone()) {
+                        phoneLayout.error = null
+                        confirmButton.isVisible = true
+                        phoneEdit.closeKeyboard()
+                    } else {
+                        confirmButton.isVisible = false
+                        phoneLayout.error = "전화번호 형식이 다릅니다."
+                    }
+
                 }
             }
 
             phoneEdit.setOnEditorActionListener(EditorInfo.IME_ACTION_DONE) {
-                if (phoneEdit.length() > 9) {
-                    confirmButton.isVisible = true
-                    phoneEdit.closeKeyboard()
-                }
+                confirmButton.isVisible = phoneEdit.length() > 9 && validPhone()
+                phoneEdit.closeKeyboard()
             }
         }
+    }
+
+    fun onClickDone() {
+        if (!validName()) {
+            binding.nameLayout.error = "1자이상의 한글을 입력해주세요."
+            return
+        }
+
+        if (!validBirthDay()) {
+            binding.birthdayLayout.error = "생년월일 형식이 다릅니다."
+            return
+        }
+
+        if (!validPhone()) {
+            binding.phoneLayout.error = "전화번호 형식이 다릅니다."
+            return
+        }
+
+
     }
 
     private fun EditText.setOnEditorActionListener(action: Int, invoke: () -> Unit) {
@@ -90,10 +146,26 @@ class IdentityInputActivity : AppCompatActivity() {
         }, 200)
     }
 
-   private fun EditText.closeKeyboard() {
+    private fun EditText.closeKeyboard() {
         this.clearFocus()
         val inputMethodManager =
             this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(this.windowToken, 0)
+    }
+
+    private fun validName() = !binding.nameEdit.text.isNullOrBlank()
+            && REGEX_NAME.toRegex().matches(binding.nameEdit.text!!)
+
+    private fun validBirthDay() = !binding.birthdayEdit.text.isNullOrBlank()
+            && REGEX_BIRTHDAY.toRegex().matches(binding.birthdayEdit.text!!)
+
+    private fun validPhone() = !binding.phoneEdit.text.isNullOrEmpty()
+            && REGEX_PHONE.toRegex().matches(binding.phoneEdit.text!!)
+
+    companion object {
+        private const val REGEX_NAME = "^[가-힣]{2,}\$"
+        private const val REGEX_BIRTHDAY =
+            "^(19|20)[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|[1,2][0-9]|3[0,1])"
+        private const val REGEX_PHONE = "^01([016789])([0-9]{3,4})([0-9]{4})\$"
     }
 }
